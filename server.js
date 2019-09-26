05a48f5b4d69b2cb0b48467d6a41e583142e7bcd
armangrigoryan96 <arman.grigoryan.96@mail.ru>
2019-09-26 18:16:00 +0400

    initialise:

---
+++ server.js
@@ -0,0 +1,33 @@
+const WebSocketServer = require("ws").Server;
+const wss = new WebSocketServer({ port: 9090 });
+
+let clients = [];
+console.log("all is good");
+
+wss.on("connection", connection => {
+  clients.push(connection);
+  broadcast({ username: "Admin", message: "A user has entered the room " });
+  connection.on("message", message => {
+    const data = JSON.parse(message);
+    broadcast(data);
+  });
+});
+
+setInterval(cleanUp, 100);
+
+const broadcast = message => {
+  const data = JSON.stringify(message);
+  clients.forEach(client => {
+    client.send(data);
+  });
+};
+
+function cleanUp() {
+  const clientsLeaving = clients.filter(
+    client => client.readyState === client.CLOSED
+  );
+  clients = clients.filter(client => client.readyState !== client.CLOSED);
+  clientsLeaving.forEach(client =>
+    broadcast({ username: "admin", message: "A user has left the room" })
+  );
+}
