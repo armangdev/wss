7257d5e02209a1f96dbb96304c0aaa581b0b0297
armangrigoryan96 <arman.grigoryan.96@mail.ru>
2019-09-26 18:36:51 +0400

    some changes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const WebSocketServer = require("ws").Server;
 const wss = new WebSocketServer({ port: 9090 });
+const port = process.env.PORT || 3000
+
 
 let clients = [];
 console.log("all is good");
@@ -31,3 +33,15 @@ function cleanUp() {
     broadcast({ username: "admin", message: "A user has left the room" })
   );
 }
+
+if( process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"))
+  app.get("*", (req, res)=> {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
+// 
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is on ${PORT}`));
